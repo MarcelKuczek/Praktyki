f58238b2bba521a55ab7a61ff48578a49d732208
dom <test@example.com>
2024-09-05 11:29:29 +0200

    3

--- CheckingComponents/lib.py
+++ CheckingComponents/lib.py
@@ -5,8 +5,22 @@ import wmi
 
 def system_version():
     return platform.platform()
-def processor_stats():
-    return platform.processor()
+
+class ProcessorInfo:
+    def __init__(self):
+        self.c = wmi.WMI()
+        self.processor = self._get_processor()
+
+    def get_name(self):
+        return self.processor.Name
+
+    def get_max_cloack_speed(self):
+        return self.processor.MaxClockSpeed
+
+    def display_processor_info(self):
+        print("Processor: ")
+        print("Name: " + str(self.get_name()))
+        print("Max clock speed: " + str(self.get_max_cloack_speed()))
 
 class RAMInfo:
     memory = psutil.virtual_memory()
@@ -25,8 +39,11 @@ if __name__ == '__main__':
     print("------------------------------------------------------------------------")
     system_version = system_version()
     print("System: " + str(system_version))
-    processor_stats = processor_stats()
-    print("Processor: " + str(processor_stats))
+
+    processor_stats = ProcessorInfo()
+    processor_stats.display_processor_info()
+
+
     RAM_stats = RAMInfo()
     print("RAM: ")
     for memory in RAM_stats.c.Win32_PhysicalMemory():
@@ -36,6 +53,7 @@ if __name__ == '__main__':
     print("Total memory: " + str(round(RAM_stats.total_memory, 3)) + "GB")
     print("Total available: " + str(round(RAM_stats.available_memory, 3)) + "GB")
     print("Total used: " + str(round(RAM_stats.used_memory, 3)) + "GB \n")
+    print("Host: ")
     host_name = host_name()
     print("Host name: " + str(host_name))
     host_address = host_address()
