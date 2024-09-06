557c1aad0d4c82c7430bf3740c095fdad877fc30
dom <test@example.com>
2024-09-06 09:58:20 +0200

    8

--- Regex/Regex.py
+++ Regex/Regex.py
@@ -1,6 +1,9 @@
 import re
-pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9]+$"
-text = " Moj numer telefonu to:  123-321-312 a moj email to: mkuczek@altcompit.pl"
+pattern = r"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9]+, \d{3}-\d{3}-\d{3}"
+text = "  Moj email to: mkuczek@altcompit.pl. Moj numer telefonu to:  123-321-312"
 
-email = re.search(pattern, text)
-print(email)
+emailfon = re.search(pattern, text)
+email = emailfon.group(1)
+fon = emailfon.group(2)
+print("email: " + str(email))
+print("fon: " + str(fon))
