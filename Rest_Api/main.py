d2a02c50d6fab9601700a03e13e7143a29bfcb8d
dom <test@example.com>
2024-09-09 09:59:19 +0200

    apidelete2

--- Rest_Api/main.py
+++ Rest_Api/main.py
@@ -1,6 +1,4 @@
 from flask import Flask, jsonify, abort
-
-from Rest_Api.data import product1
 from data import products_list
 
 app = Flask(__name__)
@@ -21,10 +19,9 @@ def get_products(product_id):
 
 @app.route('/delete/product/<int:product_id>', methods = ['DELETE'])
 def delete_products(product_id):
-    try:
-        del products_list[product_id]
-    except:
-        abort(404)
+    product = next((item for item in products_list if item.id == product_id), None)
+    if product:
+        products_list = [item for item in products_list if item["id"] != product_id]
 
 if __name__ == '__main__':
     app.run(port = 3000, debug = True)
