678f93771de25a88d16008f042b218a8597f4af1
dom <test@example.com>
2024-09-09 11:57:36 +0200

    api_data_base_3

--- Rest_Api_Data_Base/main.py
+++ Rest_Api_Data_Base/main.py
@@ -1,3 +1,5 @@
+from idlelib.rpc import request_queue
+
 from flask import Flask, jsonify, request, abort
 from flask_mysqldb import MySQL
 
@@ -6,7 +8,7 @@ app = Flask(__name__)
 app.config["MYSQL_HOST"] = 'localhost'
 app.config["MYSQL_USER"] = 'root'
 app.config["MYSQL_PASSWORD"] = ''
-app.config["MYSQL_DB"] = 'flask_db'
+app.config["MYSQL_DB"] = 'market'
 
 mysql = MySQL(app)
 
@@ -17,8 +19,38 @@ def get_products_list():
     results = cur.fetchall()
     cur.close()
 
-    products = [{'id': = row[0], 'name': = row[1], 'weight': = row[2], 'price': = row[3]} for row in results]
+    products = [{'id': row[0], 'name': row[1], 'weight': row[2], 'price': row[3]} for row in results]
     return jsonify(products)
 
-if __name__ = '__main__':
+@app.route('/product/<int:product_id>', methods=['GET'])
+def get_product(product_id):
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM products WHERE id = %s', (product_id,))
+    result = cur.fetchone()
+    cur.close()
+
+    if result:
+        products = [{'id': result[0], 'name': result[1], 'weight': result[2], 'price': result[3]}]
+        return jsonify(products)
+    else:
+        abort(404)
+
+@app.route('/product', methods=['POST'])
+def add_product():
+    if not request.json or 'name' in request.json or 'weight' in request.json or 'price' in request.json:
+       abort(404)
+
+    data = request.json
+    name = data['name']
+    weight = data['weight']
+    price = data['price']
+
+    cur = mysql.connection.cursor()
+    cur.execute('INSERT INTO products(name,weight,price)  VALUES (%s, %s, %s)', (name, weight, price))
+    mysql.connection.cursor()
+    cur.close()
+
+    return jsonify({'message:': 'Product added successfully'}), 201
+
+if __name__ == '__main__':
     app.run(port = 3000, debug = True)
