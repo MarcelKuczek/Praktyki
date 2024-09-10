7d8f9511fe448678671d7781e0de030e74685422
dom <test@example.com>
2024-09-10 10:50:37 +0200

    MVC_OAUTH2 1

---
+++ MVC_OAUTH2/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    username = 'test'
+    return render_template('test.html')
+
+
+if __name__ == '__main__':
+    app.run()
